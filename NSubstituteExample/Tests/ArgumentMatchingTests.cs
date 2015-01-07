017c78f2fc8f12c2b836028a890a809d5d7b7e8f
James Heywood <jdheywood@yahoo.co.uk>
2015-01-07 17:01:34 +0000

    Arg matching and exception throwing example tests

--- NSubstituteExample/Tests/ArgumentMatchingTests.cs
+++ NSubstituteExample/Tests/ArgumentMatchingTests.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections;
-using System.Linq;
+﻿using System.Linq;
 using NSubstitute;
 using NSubstituteExample.Interfaces;
 using NUnit.Framework;
---
+++ NSubstituteExample/Tests/ExceptionTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using NSubstitute;
+using NSubstituteExample.Interfaces;
+using NUnit.Framework;
+
+namespace NSubstituteExample.Tests
+{
+    public class ExceptionTests
+    {
+        private ICalculator calculator;
+
+        [SetUp]
+        public void Setup()
+        {
+            calculator = Substitute.For<ICalculator>();
+        }
+
+        [Test]
+        public void ThrowException()
+        {
+            // non-voids
+            calculator.Add(-1, -1).Returns(x => { throw new Exception(); });
+
+            // voids and non-voids
+            calculator
+                .When(x => x.Add(-2, -2))
+                .Do(x => { throw new Exception(); });
+
+            // both calls will throw an exception
+            Assert.Throws<Exception>(() => calculator.Add(-1, -1));
+            Assert.Throws<Exception>(() => calculator.Add(-2, -2));
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            calculator = null;
+        }
+    }
+}
